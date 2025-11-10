6c7ee6337021c28b7b0dafcc031b39b0e3a16f59
ankitcrewman <adarsh.maurya@crewman.in>
2025-11-10 16:10:21 +0530

    chat admin.py

--- chat/admin.py
+++ chat/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Chat,ChatSession,GroupDetails,Group
+from .models import Chat,ChatSession,GroupDetails,Group,Notification
 
 admin.site.register(Chat)
 admin.site.register(ChatSession)
 admin.site.register(Group)
 admin.site.register(GroupDetails)
+admin.site.register(Notification)
 # Register your models here.
