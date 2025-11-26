32b882df79b829d8f1c7332f43759af000e96dcd
shivam-kumar-sain <shivamkumar6399029@gmail.com>
2025-11-26 17:15:57 +0530

    changes in agentauth

--- emailsetup/serializers.py
+++ emailsetup/serializers.py
@@ -7,11 +7,21 @@ class EmailTemplateSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class AgentAuthenticationSerializer(serializers.ModelSerializer):
-    phone = PhoneNumberField(region="IN")  # Ensures valid phone format
+    phone = PhoneNumberField(region="IN")
+    company_name = serializers.SerializerMethodField()
+    branch_name = serializers.SerializerMethodField()
 
     class Meta:
         model = AgentAuthentication
         fields = '__all__'
+        # Add extra fields:
+        extra_fields = ['company_name', 'branch_name']
+
+    def get_company_name(self, obj):
+        return obj.company.name if obj.company else None
+
+    def get_branch_name(self, obj):
+        return obj.branch.name if obj.branch else None
 
 
 class AgentReportSerializer(serializers.ModelSerializer):
