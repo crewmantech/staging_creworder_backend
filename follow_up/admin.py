1d66639c401845c4844eaa7e5b8ed7897077f0b8
shivam-kumar-sain <shivamkumar6399029@gmail.com>
2025-12-20 14:33:05 +0530

    chags ig

--- follow_up/admin.py
+++ follow_up/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Follow_Up,Notepad,Appointment
+from .models import Follow_Up,Notepad,Appointment,Appointment_layout
 
 admin.site.register(Follow_Up)
 admin.site.register(Notepad)
 admin.site.register(Appointment)
+admin.site.register(Appointment_layout)
