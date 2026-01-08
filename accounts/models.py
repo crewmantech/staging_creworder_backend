d947a36b4e0f2dd48b30ccfb4e36a1af3cce5b3b
shivam-kumar-sain <shivamkumar6399029@gmail.com>
2026-01-08 15:51:48 +0530

    supoort ticker

--- accounts/models.py
+++ accounts/models.py
@@ -440,11 +440,8 @@ class SupportTicket(BaseModel):
     priority = models.CharField(max_length=15, blank=True, null=True, choices=priority_choices)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
-    solution = models.TextField(
-        blank=True,
-        null=True,
-        related_name="solution_support_ticket"
-    )
+    solution = models.TextField(blank=True,null=True)
+
     def save(self, *args, **kwargs):
         if not self.ticket_id:
             self.ticket_id = self.generate_ticket_id()
