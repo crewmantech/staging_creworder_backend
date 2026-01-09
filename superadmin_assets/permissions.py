523a8b15fca1e9aaba601035559fc67492d58ce8
shivam-kumar-sain <shivamkumar6399029@gmail.com>
2026-01-09 13:01:26 +0530

    changes in support ticket

--- superadmin_assets/permissions.py
+++ superadmin_assets/permissions.py
@@ -3,12 +3,11 @@ from rest_framework.permissions import BasePermission
 
 class IsSuperAdmin(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.is_superadmin
-
+        return request.user.is_authenticated and request.user.profile.user_type == 'superadmin'
 
 class IsAssignedOrSuperAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
-            request.user.is_superadmin or
+            request.user.profile.user_type == 'superadmin' or
             obj.assigned_to == request.user
         )
