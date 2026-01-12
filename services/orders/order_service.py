8cb3f02c96994d827f22f037410c38d884a0bbca
shivam-kumar-sain <shivamkumar6399029@gmail.com>
2026-01-12 16:37:09 +0530

    changes in appoienment status bulk update

--- services/orders/order_service.py
+++ services/orders/order_service.py
@@ -653,13 +653,15 @@ def checkServiceability(branch_id,company_id,data):
                 "weight": 0.5,
                 "cod": 1
                 })
-                print(payload,"------------------------------------655")
+                print(payload,"------------------------------------655",token)
                 headers = {
                 'Content-Type': 'application/json',
                 'Authorization': f'Bearer {token}'
                 }
                 response = requests.request("GET", url, headers=headers, data=payload)
-                print(response,"-------------------------------660",response.json())
+                
+                print("STATUS:", response.status_code)
+                print("RESPONSE:", response.json())
                 a=0
                 shortestDayData={}
                 cutomerCity=''
