62ae4309a6b7a8d11a03e6c8387d47b7c30551e8
shivam-kumar-sain <shivamkumar6399029@gmail.com>
2026-01-20 16:38:37 +0530

    chages in monthly

--- orders/scheduler.py
+++ orders/scheduler.py
@@ -36,7 +36,7 @@ def start_scheduler():
     scheduler.add_job(
         monthly_order_report_job,
         # CronTrigger(hour=20, minute=30),
-        CronTrigger(hour=16, minute=37),
+        CronTrigger(hour=16, minute=39),
         id="monthly_order_report",
         replace_existing=True,
     )
