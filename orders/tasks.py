3f4a1b86c78cc0d9c4917e887ad824a6184b3137
shivam-kumar-sain <shivamkumar6399029@gmail.com>
2026-01-20 14:40:20 +0530

    schduler

--- orders/tasks.py
+++ orders/tasks.py
@@ -8,21 +8,21 @@ from orders.utils import send_order_report
 
 
 def daily_order_report_job():
-    print("gag Order Report Job Started")
+    print("daily_order_report_job Order Report Job Started")
     for company in Company.objects.filter(status=True):
         for branch in company.branches.all():
             send_order_report(company, branch, "daily")
 
 
 def weekly_order_report_job():
-    print("gag Order Report Job Started")
+    print("weekly_order_report_job Order Report Job Started")
     for company in Company.objects.filter(status=True):
         for branch in company.branches.all():
             send_order_report(company, branch, "weekly")
 
 
 def monthly_order_report_job():
-    print("gag Order Report Job Started")
+    print("monthly_order_report_job Order Report Job Started")
     today = date.today()
     last_day = calendar.monthrange(today.year, today.month)[1]
 
