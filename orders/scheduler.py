f6816886ba944d923dbcdd9a6c8663b5d05bf7d2
shivam-kumar-sain <shivamkumar6399029@gmail.com>
2026-01-20 15:23:17 +0530

    schduler

--- orders/scheduler.py
+++ orders/scheduler.py
@@ -36,7 +36,7 @@ def start_scheduler():
     scheduler.add_job(
         monthly_order_report_job,
         # CronTrigger(hour=20, minute=30),
-        CronTrigger(hour=14, minute=44),
+        CronTrigger(hour=15, minute=25),
         id="monthly_order_report",
         replace_existing=True,
     )
