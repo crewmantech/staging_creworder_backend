3bcf5c48e09a093e40cbd638c7c72c5c70770910
shivam-kumar-sain <shivamkumar6399029@gmail.com>
2026-02-03 17:22:45 +0530

    changes in serilizer fo call id

--- cloud_telephony/admin.py
+++ cloud_telephony/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import CallLog, CloudTelephonyChannel,CloudTelephonyChannelAssign,UserMailSetup,CloudTelephonyVendor
+from .models import CallLog, CloudTelephonyChannel,CloudTelephonyChannelAssign,UserMailSetup,CloudTelephonyVendor,CallActivity
 admin.site.register(CloudTelephonyVendor)
 admin.site.register(CloudTelephonyChannel)
 admin.site.register(CloudTelephonyChannelAssign)
 admin.site.register(UserMailSetup)
 admin.site.register(CallLog)
+admin.site.register(CallActivity)
